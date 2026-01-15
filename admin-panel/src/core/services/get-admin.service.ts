af1a893f8bfca1f16bd36444e5cb3fc8ab8bb853
vinay-kumar-arya <vinaykumararya007@gmail.com>
2026-01-15 23:01:58 +0530

    >> production ready

--- admin-panel/src/core/services/get-admin.service.ts
+++ admin-panel/src/core/services/get-admin.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environments.';
+import { environment } from '../../environments/environments';
 
 @Injectable({
   providedIn: 'root',
---
+++ admin-panel/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://e-commerce-backent.onrender.com/api',
+};
